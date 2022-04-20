e486a3e27ef07f3563eca797534a183a4b3b09f1
leg7 <leonard.gomez@protonmail.com>
2022-04-20 11:10:28 +0200

    Changed font size to better match my new font

--- config.def.h
+++ config.def.h
@@ -5,12 +5,12 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10",
-    "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true"
+	"monospace:size=10.5",
+	"NotoColorEmoji:pixelsize=12:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	                        /*     fg         bg       */
+				/*     fg         bg       */
 	[SchemeNorm]          = { "#eceff4", "#2e3440" },
 	[SchemeSel]           = { "#eceff4", "#4c566a" },
 	[SchemeSelHighlight]  = { "#bf616a", "#4c566a" },
